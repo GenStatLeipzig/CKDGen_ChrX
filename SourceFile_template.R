bd4917bcb247b119ef10bdcd5be99fa095308bec
pottj <pott.ne.ja@gmail.com>
2022-11-04 17:34:14 +0100

    update sourceFile template

--- SourceFile_template.R
+++ SourceFile_template.R
@@ -11,12 +11,6 @@ projectpath_main = "/PATH/TO/PROJECTS/CKDGen_ChrX/scripts/"
 path_data = "/PATH/TO/PROJECTS/CKDGen_ChrX/data/"
 
 #############################
-# Own data to use
-#############################
-dataSourcePath = paste0(projectpath, "/data/")
-
-
-#############################
 # R library and R packages
 #############################
 .libPaths("/PATH/TO/RLibrary/VERSION_4.x/") 
@@ -31,6 +25,15 @@ suppressPackageStartupMessages(library(readxl))
 suppressPackageStartupMessages(library(WriteXLS))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(coloc))
+suppressPackageStartupMessages(library(biomaRt))
+suppressPackageStartupMessages(library(GenomicRanges))
+suppressPackageStartupMessages(library(rtracklayer))
+suppressPackageStartupMessages(library(doParallel))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(ggcorrplot))
+suppressPackageStartupMessages(library(RColorBrewer))
+suppressPackageStartupMessages(library(ggrepel))
+suppressPackageStartupMessages(library(gtx))
 
 #############################
 # Other Tools 
@@ -39,6 +42,9 @@ path_plink2 = "/PATH/TO/TOOLS/plink2.0/20210203/unix_64/plink2"
 path_gcta = "/PATH/TO/TOOLS/gcta_v1.94.0/gcta"
 
 #############################
-# Downloaded data sets 
+# Downloaded/requested data sets 
 #############################
 path_GTExv8 = "/PATH/TO/DATA/GTEx_v8/"
+path_NephQTL = "/PATH/TO/DATA/NEPTUNE/"
+path_HUNT = "/PATH/TO/DATA/HUNT/"
+path_UKBB = "/PATH/TO/DATA/UKBB/"
