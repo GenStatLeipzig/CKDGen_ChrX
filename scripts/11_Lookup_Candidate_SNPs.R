f1736b6242c672767857b53a1b2272d913bbdd7c
KatrinHorn <katrin.horn@imise.uni-leipzig.de>
2022-11-14 16:04:33 +0100

    Change in the phenotypes being considered decision for replication status added (done by hand)

--- scripts/11_Lookup_Candidate_SNPs.R
+++ scripts/11_Lookup_Candidate_SNPs.R
@@ -43,9 +43,8 @@ knownGWAS = rbindlist(list(graham, sakaue, kanai))
 knownGWAS
 
 #keep only kidney related phenotypes
-phenotypes2keep = c("Creatinine levels", "Estimated glomerular filtration rate", "Albumin-globulin ratio", "Creatinine levels", 
-                    "Glomerular filtration rate", "Non-albumin protein levels", "Nephrotic syndrome", "Serum albumin levels", 
-                    "Blood urea nitrogen levels", "Serum uric acid levels", "Serum creatinine levels", "Serum total protein level")
+phenotypes2keep = c("Creatinine levels", "Estimated glomerular filtration rate", "Glomerular filtration rate", 
+                    "Blood urea nitrogen levels", "Serum uric acid levels", "Serum creatinine levels")
 
 knownGWAS = knownGWAS[is.element(`DISEASE/TRAIT`, phenotypes2keep), ]
 table(knownGWAS[, `DISEASE/TRAIT`])
@@ -83,8 +82,8 @@ MA[, shortID := shortID]
 
 
 #' # Look up of known GWAS hits in our results
-cols2keep = c("STUDY", "DISEASE/TRAIT", "INITIAL SAMPLE SIZE", "REGION" , "CHR_ID", "CHR_POS", "REPORTED GENE(S)", "MAPPED_GENE", "STRONGEST SNP-RISK ALLELE",
-              "SNPS", "RISK ALLELE FREQUENCY", "P-VALUE", "PVALUE_MLOG", "OR or BETA")
+cols2keep = c("STUDY", "DISEASE/TRAIT", "INITIAL SAMPLE SIZE", "REGION" , "CHR_ID", "CHR_POS", "REPORTED GENE(S)", "MAPPED_GENE", 
+              "STRONGEST SNP-RISK ALLELE", "SNPS", "RISK ALLELE FREQUENCY", "P-VALUE", "PVALUE_MLOG", "OR or BETA")
 colsOut = setdiff(colnames(knownGWAS), cols2keep)
 knownGWAS[, get("colsOut") := NULL]
 
@@ -150,14 +149,135 @@ knownGWAS = cbind(knownGWAS, MA)
 #(big table)
 write.table(knownGWAS, file = "../results/11_Look_Up_GWAS_hits_results.txt", col.names = T, row.names = F)
 
-#smaller table
-cols2keep = c("STUDY", "DISEASE/TRAIT", "INITIAL SAMPLE SIZE", "STRONGEST SNP-RISK ALLELE", "SNPS", "CHR_ID", "CHR_POS", "RISK ALLELE FREQUENCY", "P-VALUE",
+#smaller table with only eGFR, BUN and UA
+#plus checks for reproducing hits
+cols2keep = c("STUDY", "DISEASE/TRAIT", "INITIAL SAMPLE SIZE", "STRONGEST SNP-RISK ALLELE", "SNPS", "CHR_ID", "CHR_POS", "RISK ALLELE FREQUENCY", "P-VALUE", "OR or BETA", 
               "eGFR.N", "eGFR.effect_allele", "eGFR.EAF", "eGFR.infoScore", "eGFR.I2", "eGFR.beta", "eGFR.SE", "eGFR.P", "eGFR.logP", "eGFR.invalid_assoc", 
-              "eGFR.reason_for_exclusion", "UA.N", "UA.effect_allele", "UA.EAF", "UA.infoScore", "UA.I2", "UA.beta", "UA.SE", "UA.P", "UA.logP", 
-              "UA.invalid_assoc", "UA.reason_for_exclusion")
+              "eGFR.reason_for_exclusion", "BUN.N", "BUN.effect_allele", "BUN.EAF", "BUN.infoScore", "BUN.I2", "BUN.beta", "BUN.SE", "BUN.P", "BUN.logP",
+              "BUN.invalid_assoc", "BUN.reason_for_exclusion", "UA.N", "UA.effect_allele", "UA.EAF", "UA.infoScore", "UA.I2", "UA.beta", "UA.SE", "UA.P", 
+              "UA.logP", "UA.invalid_assoc", "UA.reason_for_exclusion")
 colsOut = setdiff(colnames(knownGWAS), cols2keep)
 knownGWAS[, get("colsOut") := NULL]
-write.table(knownGWAS, file = "../results/11_Look_Up_GWAS_hits_eGFR_UA_only.txt", col.names = T, row.names = F)
+
+#check alleles
+dummy = strsplit(knownGWAS[, `STRONGEST SNP-RISK ALLELE`], split = "-")
+knownGWAS[, RISK_ALLELE := sapply(dummy, function(x) return(x[[2]]))]
+knownGWAS[, same_allele_eGFR := RISK_ALLELE == eGFR.effect_allele]
+knownGWAS[, same_allele_BUN := RISK_ALLELE == BUN.effect_allele]
+knownGWAS[, same_allele_UA := RISK_ALLELE == UA.effect_allele]
+#-> no allele given where check ist false
+
+#do check by hand
+knownGWAS[, SNP_replicated := NA]
+knownGWAS[, `OR or BETA` := as.character(`OR or BETA`)]
+#eGFR, same allele, p < 0.05, 5.72 z score decrease -> same beta direction -> replicated
+knownGWAS[1, SNP_replicated := 1]
+knownGWAS[1, `OR or BETA` := "5.72 z score decrease"]
+#eGFR, same allele, p < 0.05, 9.266 z score decrease -> same beta direction -> replicated
+knownGWAS[2, SNP_replicated := 1]
+knownGWAS[2, `OR or BETA` := "9.266 z score decrease"]
+#creatinine, same allele, p < 0.05, 0.188414 unit increase -> different beta direction -> replicated with eGFR
+knownGWAS[4, SNP_replicated := 1]
+knownGWAS[4, `OR or BETA` := "0.188414 unit increase"]
+#eGFR, same allele, p < 0.05, 5.454 z score increase -> same beta direction -> replicated
+knownGWAS[7, SNP_replicated := 1]
+knownGWAS[7, `OR or BETA` := "5.454 z score increase"]
+#BUN, same allele, p < 0.05, 0.0132 unit increase -> same beta direction -> replicated
+knownGWAS[9, SNP_replicated := 1]
+knownGWAS[9, `OR or BETA` := "0.0132 unit increase"]
+#BUN, same allele, p < 0.05, 0.014 unit increase -> same beta direction -> replicated
+knownGWAS[11, SNP_replicated := 1]
+knownGWAS[11, `OR or BETA` := "0.014 unit increase"]
+#BUN, same allele, p < 0.05, 0.0118 unit decrease -> same beta direction -> replicated
+knownGWAS[12, SNP_replicated := 1]
+knownGWAS[12, `OR or BETA` := "0.0118 unit decrease"]
+#BUN, same allele, p < 0.05, 0.011 unit increase -> same beta direction -> replicated
+knownGWAS[13, SNP_replicated := 1]
+knownGWAS[13, `OR or BETA` := "0.011 unit increase"]
+#BUN, same allele, p < 0.05, 0.013 unit increase -> same beta direction -> replicated
+knownGWAS[14, SNP_replicated := 1]
+knownGWAS[14, `OR or BETA` := "0.013 unit increase"]
+#UA, same allele, p > 0.05, 0.0184248 unit decrease -> same beta direction -> not replicated
+knownGWAS[15, SNP_replicated := 0]
+knownGWAS[15, `OR or BETA` := "0.0184248 unit decrease"]
+#creatinine, same allele, p < 0.05, 0.0157643 unit decrease -> different beta direction -> replicated with eGFR
+knownGWAS[16, SNP_replicated := 1]
+knownGWAS[16, `OR or BETA` := "0.0157643 unit decrease"]
+#creatinine, same allele, p < 0.05, 0.0157191 unit decrease -> different beta direction -> replicated with eGFR
+knownGWAS[17, SNP_replicated := 1]
+knownGWAS[17, `OR or BETA` := "0.0157191 unit decrease"]
+#UA, same allele, p < 0.05, 0.0138 unit increase -> same beta direction -> replicated
+knownGWAS[18, SNP_replicated := 1]
+knownGWAS[18, `OR or BETA` := "0.0138 unit increase"]
+#UA, same allele, p < 0.05, 0.0094 unit decrease -> same beta direction -> replicated
+knownGWAS[19, SNP_replicated := 1]
+knownGWAS[19, `OR or BETA` := "0.0094 unit decrease"]
+#UA, same allele, p < 0.05, 0.013 unit decrease -> same beta direction -> replicated
+knownGWAS[20, SNP_replicated := 1]
+knownGWAS[20, `OR or BETA` := "0.013 unit decrease"]
+#UA, same allele, p < 0.05, 0.0094 unit decrease -> same beta direction -> replicated
+knownGWAS[21, SNP_replicated := 1]
+knownGWAS[21, `OR or BETA` := "0.0094 unit decrease"]
+#creatinine, same alleles, p < 0.05, 0.0138 unit decrease -> different beta direction -> replicated by eGFR
+knownGWAS[23, SNP_replicated := 1]
+knownGWAS[23, `OR or BETA` := "0.0138 unit decrease"]
+#creatinine, same allele, p < 0.05, 0.0125 unit decrease -> different beta direction -> replicated by eGFR
+knownGWAS[25, SNP_replicated := 1]
+knownGWAS[25, `OR or BETA` := "0.0125 unit decrease"]
+#creatinine, same allele, p < 0.05, 0.0128 unit increase -> different beta direction -> replicated by eGFR
+knownGWAS[26, SNP_replicated := 1]
+knownGWAS[26, `OR or BETA` := "0.0128 unit increase"]
+#creatinine, same allele, p < 0.05, 0.0125 unit increase -> different beta direction -> replicated by eGFR
+knownGWAS[27, SNP_replicated := 1]
+knownGWAS[27, `OR or BETA` := "0.0125 unit increase"]
+#creatinine, same allele, p < 0.05, 0.1699 unit increase -> different beta direction -> replicated by eGFR
+knownGWAS[28, SNP_replicated := 1]
+knownGWAS[28, `OR or BETA` := "0.1699 unit increase"]
+#creatinine, same allele, p < 0.05, 0.0111 unit increase -> different beta direction -> replicated by eGFR
+knownGWAS[30, SNP_replicated := 1]
+knownGWAS[30, `OR or BETA` := "0.0111 unit increase"]
+#creatinine, same allele, p < 0.05, 0.014 unit decrease -> different beta direction -> replicated by eGFR
+knownGWAS[32, SNP_replicated := 1]
+knownGWAS[32, `OR or BETA` := "0.014 unit decrease"]
+#creatinine, same allele, p < 0.05, 0.0167 unit decrease -> different beta direction -> replicated by eGFR
+knownGWAS[33, SNP_replicated := 1]
+knownGWAS[33, `OR or BETA` := "0.0167 unit decrease"]
+#creatinine, same allele, p < 0.05, 0.0138 unit increase -> different beta direction -> replicated by eGFR
+knownGWAS[34, SNP_replicated := 1]
+knownGWAS[34, `OR or BETA` := "0.0138 unit increase"]
+#creatinine, same allele, p < 0.05, 0.0178 unit decrease -> different beta direction -> replicated by eGFR
+knownGWAS[36, SNP_replicated := 1]
+knownGWAS[36, `OR or BETA` := "0.0178 unit decrease"]
+#creatinine, same allele, p < 0.05, 0.0102 unit increase -> different beta direction -> replicated by eGFR
+knownGWAS[37, SNP_replicated := 1]
+knownGWAS[37, `OR or BETA` := "0.0102 unit increase"]
+#creatinine, same allele, p < 0.05, 0.0104 unit decrease -> different beta direction -> replicated by eGFR
+knownGWAS[38, SNP_replicated := 1]
+knownGWAS[38, `OR or BETA` := "0.0104 unit decrease"]
+#UA, same allele, p < 0.05, 0.0167 unit increase -> same beta direction -> replicated
+knownGWAS[39, SNP_replicated := 1]
+knownGWAS[39, `OR or BETA` := "0.0167 unit increase"]
+#UA, same alleles, p 0.05, 0.0145 unit decrease -> same beta direction -> replicated
+knownGWAS[40, SNP_replicated := 1]
+knownGWAS[40, `OR or BETA` := "0.0145 unit decrease"]
+#UA, same allele, p < 0.05, 0.0114 unit increase -> same beta direction -> replicated
+knownGWAS[41, SNP_replicated := 1]
+knownGWAS[41, `OR or BETA` := "0.0114 unit increase"]
+#UA, same alleles, p < 0.05, 0.0118 unit increase -> same beta direction -> replicated
+knownGWAS[42, SNP_replicated := 1]
+knownGWAS[42, `OR or BETA` := "0.0118 unit increase"]
+#creatinine, no risk allele given, p < 0.05, 0.02069 unit decrease -> replicated
+knownGWAS[44, SNP_replicated := 1]
+knownGWAS[44, `OR or BETA` := "0.02069 unit decrease"]
+#(e)GFR, no risk allele given, p < 0.05, 	0.02271 unit increase -> replicated
+knownGWAS[46, SNP_replicated := 1]
+knownGWAS[46, `OR or BETA` := "0.02271 unit increase"]
+
+table(knownGWAS[, SNP_replicated], useNA = "ifany") 
+
+knownGWAS[, c("same_allele_eGFR", "same_allele_BUN", "same_allele_UA", "RISK_ALLELE") := NULL]
+
+write.table(knownGWAS, file = "../results/11_Look_Up_GWAS_hits_eGFR_BUN_UA_only.txt", col.names = T, row.names = F, sep = "|")
 
 #input for annotation pipeline
 step25 = fread("../results/05_b_step25_Matching_Table_UKBB.txt.gz")
@@ -173,7 +293,7 @@ knownGWAS[, CHR_ID := NULL]
 knownGWAS[, CHR_POS := NULL]
 setcolorder(knownGWAS, c("snp", "chr", "position", "STUDY", "DISEASE/TRAIT", "INITIAL SAMPLE SIZE", "STRONGEST SNP-RISK ALLELE", "SNPS", 
                          "RISK ALLELE FREQUENCY", "P-VALUE"))
-write.table(knownGWAS, file = "../../../10_metaGWAS/_results_combined/Lookup_GWAS_Hits/SNPs_to_annotate.txt", col.names = T, row.names = F)
+write.table(knownGWAS, file = "../../../10_metaGWAS/_results_combined/Lookup_GWAS_Hits/SNPs_to_annotate_v3.txt", col.names = T, row.names = F)
 
 
 #' # Session Info ####
